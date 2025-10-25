2b740981b1740fdf7719df8e0c5d4df198a9cb08
BishBallet <113387961+BishBallet@users.noreply.github.com>
2025-10-26 06:09:18 +0800

    Delete CCS101 directory

--- CCS101/index.js
+++
@@ -1,6 +0,0 @@
-let ansName;
-
-document.getElementById("submit").onclick = function(){
-    ansName = document.getElementById("name").value;
-    document.getElementById("ans-name").textContent = '$(ansName)'
-}
